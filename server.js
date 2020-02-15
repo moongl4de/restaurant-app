1af3aff559211c135d0827dea6055580442e1d2e
aagrosse <aagrock81@yahoo.com>
2020-02-15 10:47:33 -0500

    updated server

---
+++ server.js
@@ -0,0 +1,54 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Table Data (DATA)
+// =============================================================
+const tables = [
+    {
+      customerName: "yoda",
+      phoneNumber: "23456778",
+      customerEmail: "Jedi@master.netr",
+      customerID: 900,
+     
+    },
+];
+
+
+// Routes
+// =============================================================
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+  
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+
+
+
+
+
+
+  // Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
